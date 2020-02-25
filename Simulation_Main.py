3e3be87cae2f17a8e936b2b50f2d7ab20517bc97
Tom Marsh <tjmarshy1@gmail.com>
2020-02-25 19:20:57 +0000

    little updates

--- Simulation_Main.py
+++ Simulation_Main.py
@@ -16,7 +16,7 @@ fs = 2e6   #sample rate after stretch proccessing
 fss = 2e9  #sample rate to generate waveforms
 t = np.arange(0, tm, 1/fss)
 TxPower = 1e-2 #/watts  10dBm
-Gain = 1 #change to function of angle later
+Gain = 15 #change to function of angle later
 
 def RAngCalc(data,sweep_slope,fs):
     R = np.fft.fft(data,None,0)
@@ -42,7 +42,7 @@ RxArray = np.zeros((29,2))
 Temp = np.zeros((len(t),29),dtype=complex)
 RxSig = np.zeros((int(len(t)/(fss/fs)),29),dtype=complex)
 
-Reflector = [[0,3.87,1],[-0.58,3.87,1],[1.25,1.94,1],[-1.10,1.35,1]]  #each element = [x,y,rcs]  maybe z later #maybe class aswell
+Reflector = [[0,3.87,1.72467],[-0.58,3.87,1.72467],[1.25,1.94,2.587],[-1.10,1.35,0.01337]]  #each element = [x,y,rcs]  maybe z later #maybe class aswell
 
 for Obj in Reflector:
 
@@ -51,29 +51,28 @@ for Obj in Reflector:
         dist = (np.sqrt((RxArray[i][0]-Obj[0])**2 + (RxArray[i][1]-Obj[1])**2)) #find distance between reflector and array element
         tdelay = (2*dist)/c
         P_received =  (TxPower * (Gain**2) * Obj[2] * (lamb**2)) / ((4*np.pi)**3 * dist**4)   #https://www.radartutorial.eu/01.basics/The%20Radar%20Range%20Equation.en.html
-        print(P_received)
-        Temp[:,i] = np.add(Temp[:,i],np.exp(np.pi*1j*(fc*np.subtract(t,tdelay)+k*(np.subtract(t,tdelay)**2)))) # produce delayed chirp
+        Temp[:,i] = np.add(Temp[:,i],np.sqrt(P_received) * np.exp(np.pi*1j*(fc*np.subtract(t,tdelay)+k*(np.subtract(t,tdelay)**2)))) # produce delayed chirp
 
                       
 
-mix = np.conj(Temp) * np.exp(np.pi*1j*(fc*t+k*t**2))[:, np.newaxis]  #stretch process with original signal          
+mix = np.conj(Temp) * np.sqrt(TxPower)*np.exp(np.pi*1j*(fc*t+k*t**2))[:, np.newaxis]  #stretch process with original signal          
 RxSig[:,:] = mix[0:-1:int(fss/fs)][:]#Reduce sampling rate to 2e6 after stretch
 
 
 fig, ax = plt.subplots(1,2,subplot_kw=dict(projection='polar'))
 ax[0].set_theta_zero_location("N"), ax[0].set_theta_direction(-1), ax[0].set_ylim([0, 5])
-ax[0].set_xlim([-np.pi/4, np.pi/4]), ax[0].grid(False)
+ax[0].set_xlim([-np.pi/4, np.pi/4])#, ax[0].grid(False)
 
 ax[1].set_theta_zero_location("N"), ax[1].set_theta_direction(-1), ax[1].set_ylim([0, 5])
-ax[1].set_xlim([-np.pi/4, np.pi/4]), ax[1].grid(False)
+ax[1].set_xlim([-np.pi/4, np.pi/4])#, ax[1].grid(False)
 
 
 
 ## Sim Data
 [rs,asim,Ran,Ang,zs] = RAngCalc(RxSig,k,fs)           #do data processing
 
-levels = np.linspace(0,1e4,100) 
-cm = ax[0].contourf(np.radians(-asim),rs,np.abs(zs),levels=levels,cmap='plasma')
+#levels = np.linspace(0,1e4,100) 
+cm = ax[0].contourf(np.radians(-asim),rs,np.abs(zs),cmap='plasma')   #,levels=levels
 cb = fig.colorbar(cm,ax=ax[0],shrink=0.5)
 
 # Real Data
